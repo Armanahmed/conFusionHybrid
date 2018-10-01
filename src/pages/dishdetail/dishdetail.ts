1e25463f6ede6d2ab2340084c2f261b6f13fdd1f
Armanahmed <aaarmanapu@gmail.com>
2018-10-01 09:57:11 +0600

    Alerts, Toasts and Loading

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
@@ -24,7 +24,7 @@ export class DishdetailPage {
 	numcomments: number;
   favorite: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, @Inject('BaseURL') private BaseURL, private favoriteservice: FavoriteProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, @Inject('BaseURL') private BaseURL, private toastCtrl: ToastController, private favoriteservice: FavoriteProvider) {
   	this.dish = navParams.get('dish');
     this.favorite = this.favoriteservice.isFavorite(this.dish.id);
   	this.numcomments = this.dish.comments.length;
@@ -41,6 +41,9 @@ export class DishdetailPage {
   addToFavorites() {
     console.log('Adding to Favorites', this.dish.id);
     this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+    this.toastCtrl.create({
+      message: 'Dish ' + this.dish.id + ' added as a favorite successfully', position: 'middle', duration: 3000
+    }).present();
   }
 
 }
